a159782a5bf3aa38ac56baf6980c2217c5148b42
Geoff Richards <geoff@geoffrichards.co.uk>
2007-09-27 18:23:36 +0100

    Remove old-design URI modules which aren't going in version 1.0
    I'm ditching 'sip', 'sips', 'gopher', and 'mailto' because they're relatively
    complicated and little used.  They can be implemented properly in a future
    version.
    
    The new 'TODO' file lists these and others that aren't implemented yet.

--- URI/sips.lua
+++
@@ -1,8 +0,0 @@
-local M = { _MODULE_NAME = "URI.sips" }
-local URI = require "URI"
-URI._subclass_of(M, "URI.sip")
-
-function M.default_port () return 5061 end
-
-return M
--- vi:ts=4 sw=4 expandtab
