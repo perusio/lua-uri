868b854e7e96abac651b8c189b076c4a0a363e08
Geoff Richards <geoff@geoffrichards.co.uk>
2007-10-01 03:57:07 +0100

    Port 'uri.urn.oid' module to new library design
    This now validates correctly.  It doesn't normalize the URI anymore than the
    generic URN module does, because there's nothing else to be done.
    
    I've replaced the Perl module's 'oid' method with 'oid_numbers' for
    consistency with 'isbn_digits' and so on, and it now validates its input
    to preserve objects in a correct state at all times.

--- test/urn-oid.lua
+++ test/urn-oid.lua
@@ -2,26 +2,67 @@ require "uri-test"
 local URI = require "uri"
 local testcase = TestCase("Test uri.urn.oid")
 
-function testcase:test_oid ()
-    local u = URI:new("urn:oid")
-    is("urn:oid", tostring(u))
-    is("urn", u:scheme())
-    is("oid", u:nid())
-    is("", u:nss())
-    assert_array_shallow_equal({}, u:oid())
+function testcase:test_parse_and_normalize ()
+    local uri = assert(URI:new("urn:OId:1.3.50403060.0.23"))
+    is("uri.urn.oid", uri._NAME)
+    is("urn:oid:1.3.50403060.0.23", uri:uri())
+    is("urn:oid:1.3.50403060.0.23", tostring(uri))
+    is("oid", uri:nid())
+    is("1.3.50403060.0.23", uri:nss())
+    is("oid:1.3.50403060.0.23", uri:path())
+    assert_array_shallow_equal({ 1, 3, 50403060, 0, 23 }, uri:oid_numbers())
 
-    u:nss("1.2.3")
-    is("urn", u:scheme())
-    is("oid", u:nid())
-    is("1.2.3", u:nss())
-    assert_array_shallow_equal({1,2,3}, u:oid())
+    -- Examples from RFC 3061 section 3
+    uri = assert(URI:new("urn:oid:1.3.6.1"))
+    is("urn:oid:1.3.6.1", tostring(uri))
+    assert_array_shallow_equal({ 1, 3, 6, 1 }, uri:oid_numbers())
+    uri = assert(URI:new("urn:oid:1.3.6.1.4.1"))
+    is("urn:oid:1.3.6.1.4.1", tostring(uri))
+    assert_array_shallow_equal({ 1, 3, 6, 1, 4, 1 }, uri:oid_numbers())
+    uri = assert(URI:new("urn:oid:1.3.6.1.2.1.27"))
+    is("urn:oid:1.3.6.1.2.1.27", tostring(uri))
+    assert_array_shallow_equal({ 1, 3, 6, 1, 2, 1, 27 }, uri:oid_numbers())
+    uri = assert(URI:new("URN:OID:0.9.2342.19200300.100.4"))
+    is("urn:oid:0.9.2342.19200300.100.4", tostring(uri))
+    assert_array_shallow_equal({ 0, 9, 2342, 19200300, 100, 4 },
+                               uri:oid_numbers())
+end
 
-    u:oid({1,2,3,4,5,6,7,8,9,10})
-    is("urn:oid:1.2.3.4.5.6.7.8.9.10", tostring(u))
-    is("urn", u:scheme())
-    is("oid", u:nid())
-    is("1.2.3.4.5.6.7.8.9.10", u:nss())
-    assert_array_shallow_equal({1,2,3,4,5,6,7,8,9,10}, u:oid())
+function testcase:test_bad_syntax ()
+    is_bad_uri("empty nss", "urn:oid:")
+    is_bad_uri("bad character", "urn:oid:1.2.x.3")
+    is_bad_uri("missing number", "urn:oid:1.2..3")
+    is_bad_uri("leading zero", "urn:oid:1.2.03.3")
+    is_bad_uri("leading zero at start", "urn:oid:01.2.3.3")
+end
+
+function testcase:test_set_oid_numbers ()
+    local uri = assert(URI:new("urn:oid:0.1.23"))
+    assert_array_shallow_equal({ 0, 1, 23 }, uri:oid_numbers({ 1 }))
+    is("urn:oid:1", tostring(uri))
+    assert_array_shallow_equal({ 1 }, uri:oid_numbers({ 234252345, 340, 4, 0 }))
+    is("urn:oid:234252345.340.4.0", tostring(uri))
+    assert_array_shallow_equal({ 234252345, 340, 4, 0 },
+                               uri:oid_numbers({ 23.42 }))
+    is("urn:oid:23", tostring(uri))
+    assert_array_shallow_equal({ 23 }, uri:oid_numbers())
+end
+
+function testcase:test_set_bad_oid_numbers ()
+    local uri = assert(URI:new("urn:OID:0.1.23"))
+    assert_error("set OID numbers to non-table value",
+                 function () uri:oid_numbers("1") end)
+    assert_error("set OID to empty list of numbers",
+                 function () uri:oid_numbers({}) end)
+    assert_error("set OID number to negative number",
+                 function () uri:oid_numbers({ -23 }) end)
+    assert_error("set OID number array containing bad type",
+                 function () uri:oid_numbers({ "x" }) end)
+
+    -- None of that should have had any affect
+    is("urn:oid:0.1.23", tostring(uri))
+    assert_array_shallow_equal({ 0, 1, 23 }, uri:oid_numbers())
+    is("uri.urn.oid", uri._NAME)
 end
 
 lunit.run()
--- uri/urn/oid.lua
+++ uri/urn/oid.lua
@@ -1,20 +1,41 @@
--- RFC 3061
 local M = { _NAME = "uri.urn.oid" }
-local URI = require "uri"
-URI._subclass_of(M, "uri.urn")
+local Util = require "uri._util"
+local URN = require "uri.urn"
+Util.subclass_of(M, URN)
 
-function M.oid (self, new)
-    local old = self:nss()
+-- This implements RFC 3061.
+
+function M.init (self)
+    local nss = self:nss()
+    if nss == "" then return nil, "OID can't be zero-length" end
+    if not nss:find("^[.0-9]*$") then return nil, "bad character in OID" end
+    if nss:find("%.%.") then return nil, "missing number in OID" end
+    if nss:find("^0[^.]") or nss:find("%.0[^.]") then
+        return nil, "OID numbers shouldn't have leading zeros"
+    end
+    return self
+end
+
+function M.oid_numbers (self, new)
+    local old = Util.split("%.", self:nss())
+    for i = 1, #old do old[i] = tonumber(old[i]) end
+
     if new then
-        if type(new) ~= "string" then new = table.concat(new, ".") end
-        self:nss(new)
+        if type(new) ~= "table" then error("expected array of numbers") end
+        local nss = ""
+        for _, n in ipairs(new) do
+            if type(n) == "string" and n:find("^%d+$") then n = tonumber(n) end
+            if type(n) ~= "number" then error("bad type for number in OID") end
+            n = n - n % 1
+            if n < 0 then error("negative numbers not allowed in OID") end
+            if nss ~= "" then nss = nss .. "." end
+            nss = nss .. n
+        end
+        if nss == "" then error("no numbers in new OID value") end
+        self:nss(nss)
     end
 
-    local result = URI._split("%.", old)
-    for i, v in ipairs(result) do
-        result[i] = tonumber(v)
-    end
-    return result
+    return old
 end
 
 return M
