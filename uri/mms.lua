5851866bbd101900fa4b15cfe41308e0fc68fa19
Geoff Richards <geoff@geoffrichards.co.uk>
2007-11-25 11:25:34 +0000

    Remove 'uri.mms' class to avoid confusion
    There are at least two different non-standard uses of the 'mms:' scheme,
    one for the Microsoft streaming protocol, and one which uses email addresses
    or something for MMS messaging (something to do with mobile phones I think).
    Better for now I think to get rid of this.

--- uri/mms.lua
+++
@@ -1,9 +0,0 @@
-local M = { _NAME = "uri.mms" }
-local Util = require "uri._util"
-local Http = require "uri.http"
-Util.subclass_of(M, Http)
-
-function M.default_port () return 1755 end
-
-return M
--- vi:ts=4 sw=4 expandtab
