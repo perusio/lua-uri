eaa8c8d6b6f9488235514372e0e599902464837c
Geoff Richards <geoff@geoffrichards.co.uk>
2007-11-26 12:01:05 +0000

    New implementation of 'file' URI scheme modules
    I've redone this from scratch, because I think the original Perl design was
    far too complex.  I've also removed the support for some little-used operating
    systems (FAT (meaning DOS I think), OS2, Mac (before MacOS X), and QNX).
    The base class for OS-specific code has also been removed, because the two
    remaining OS classes are simple enough and different enough that there's
    no point trying to share any implementation.
    
    The new file URI code fits into my new API, and just provides functions for
    converting between operating system paths and URIs.  There is no longer any
    support for encoding relative paths in file URIs, because I don't think that
    makes any sense (a relative URI reference should be used instead).
    
    There's now documentation and a completely new (more thorough) set of tests
    for file URIs.

--- test/file.lua
+++ test/file.lua
@@ -3,58 +3,149 @@ local URI = require "uri"
 local URIFile = require "uri.file"
 local testcase = TestCase("Test uri.file")
 
-local tests = {
-    { "file",          "unix",       "win32",         "mac" },
-    -----------------  ------------  ---------------  --------------
-    { "file://localhost/foo/bar",
-                       "!/foo/bar",  "!\\foo\\bar",   "!foo:bar" },
-    { "file:///foo/bar",
-                       "/foo/bar",   "\\foo\\bar",    "!foo:bar" },
-    { "file:/foo/bar", "!/foo/bar",  "!\\foo\\bar",   "foo:bar" },
-    { "foo/bar",       "foo/bar",    "foo\\bar",      ":foo:bar" },
-    { "file://foo/bar","!//foo/bar", "!\\\\foo\\bar", "!foo:bar" },
-    { "file://a:/",    "!//a:/",     "!A:\\",         nil },
-    { "file:///A:/",   "/A:/",       "A:\\",          nil },
-    { "file:///",      "/",          "\\",            nil },
-    { ".",             ".",          ".",             ":" },
-    { "..",            "..",         "..",            "::" },
-    { "%2E",           "!.",         "!.",            ":." },
-    { "../%2E%2E",     "!../..",     "!..\\..",       "::.." },
-}
+local function test_norm (orig, expected)
+    local uri = assert(URI:new(orig))
+    is(expected, tostring(uri))
+end
 
-function testcase:test_file ()
-    local os_list = table.remove(tests, 1)
-    table.remove(os_list, 1)    -- file
+function testcase:test_normalize ()
+    test_norm("file://LocalHost/foo", "file:///foo")
+    test_norm("file://localhost/", "file:///")
+    test_norm("file://localhost", "file:///")
+    test_norm("file://", "file:///")
+    test_norm("file:/", "file:///")
+    test_norm("file:/foo", "file:///foo")
+    test_norm("file://foo", "file://foo/")
+end
 
-    for _, t in ipairs(tests) do
-        local file = table.remove(t, 1)
-        local err = 0
+function testcase:test_invalid ()
+    is_bad_uri("just scheme", "file:")
+    is_bad_uri("scheme with relative path", "file:foo/bar")
+end
 
-        local u = URI:new(file, "file")
-        for i, os in ipairs(os_list) do
-            local f = u:file(os)
-            local expect = t[i]
-            if not f then f = "<nil>" end
-            if not expect then expect = "<nil>" end
-            local loose
-            expect, loose = expect:gsub("^!", "", 1)
-            loose = loose > 0
-            if expect ~= f then
-                print("URI:new('" .. file .. "', 'file'):file('" .. os ..
-                      "') ~= " .. expect .. ", got " .. f)
-                err = err + 1
-            end
-            if t[i] and not loose then
-                local u2 = URIFile:new(t[i], os)
-                if tostring(u2) ~= file then
-                    print("URI::file->new('" .. t[i] .. "', '" .. os ..
-                          "') ne " .. file .. ", got " .. tostring(u2))
-                    err = err + 1
-                end
-            end
-        end
-        is(0, err)
-    end
+function testcase:test_set_host ()
+    local uri = assert(URI:new("file:///foo"))
+    is("", uri:host())
+    is("", uri:host("LocalHost"))
+    is("file:///foo", tostring(uri))
+    is("", uri:host("host.name"))
+    is("file://host.name/foo", tostring(uri))
+    is("host.name", uri:host(""))
+    is("file:///foo", tostring(uri))
+end
+
+function testcase:test_set_path ()
+    local uri = assert(URI:new("file:///foo"))
+    is("/foo", uri:path())
+    is("/foo", uri:path(nil))
+    is("file:///", tostring(uri))
+    is("/", uri:path(""))
+    is("file:///", tostring(uri))
+    is("/", uri:path("/bar/frob"))
+    is("file:///bar/frob", tostring(uri))
+    is("/bar/frob", uri:path("/"))
+    is("file:///", tostring(uri))
+end
+
+function testcase:test_bad_usage ()
+    local uri = assert(URI:new("file:///foo"))
+    assert_error("nil host", function () uri:host(nil) end)
+    assert_error("set userinfo", function () uri:userinfo("foo") end)
+    assert_error("set port", function () uri:userinfo(23) end)
+    assert_error("set relative path", function () uri:userinfo("foo/") end)
+end
+
+local function uri_to_fs (os, uristr, expected)
+    local uri = assert(URI:new(uristr))
+    is(expected, uri:filesystem_path(os))
+end
+
+local function fs_to_uri (os, path, expected)
+    is(expected, tostring(URIFile.make_file_uri(path, os)))
+end
+
+function testcase:test_uri_to_fs_unix ()
+    uri_to_fs("unix", "file:///", "/")
+    uri_to_fs("unix", "file:///c:", "/c:")
+    uri_to_fs("unix", "file:///C:/", "/C:/")
+    uri_to_fs("unix", "file:///C:/Program%20Files", "/C:/Program Files")
+    uri_to_fs("unix", "file:///C:/Program%20Files/", "/C:/Program Files/")
+    uri_to_fs("unix", "file:///Program%20Files/", "/Program Files/")
+end
+
+function testcase:test_uri_to_fs_unix_bad ()
+    -- On Unix platforms, there's no equivalent of UNC paths.
+    local uri = assert(URI:new("file://laptop/My%20Documents/FileSchemeURIs.doc"))
+    assert_error("Unix path with host name",
+                 function () uri:filesystem_path("unix") end)
+    -- Unix paths can't contain null bytes or encoded slashes.
+    uri = assert(URI:new("file:///frob/foo%00bar/quux"))
+    assert_error("Unix path with null byte",
+                 function () uri:filesystem_path("unix") end)
+    uri = assert(URI:new("file:///frob/foo%2Fbar/quux"))
+    assert_error("Unix path with encoded slash",
+                 function () uri:filesystem_path("unix") end)
+end
+
+function testcase:test_fs_to_uri_unix ()
+    fs_to_uri("unix", "/", "file:///")
+    fs_to_uri("unix", "//", "file:///")
+    fs_to_uri("unix", "///", "file:///")
+    fs_to_uri("unix", "/foo/bar", "file:///foo/bar")
+    fs_to_uri("unix", "/foo/bar/", "file:///foo/bar/")
+    fs_to_uri("unix", "//foo///bar//", "file:///foo/bar/")
+    fs_to_uri("unix", "/foo bar/%2F", "file:///foo%20bar/%252F")
+end
+
+function testcase:test_fs_to_uri_unix_bad ()
+    -- Relative paths can't be converted to URIs, because URIs are inherently
+    -- absolute.
+    assert_error("relative Unix path",
+                 function () FileURI.make_file_uri("foo/bar", "unix") end)
+    assert_error("relative empty Unix path",
+                 function () FileURI.make_file_uri("", "unix") end)
+end
+
+function testcase:test_uri_to_fs_win32 ()
+    uri_to_fs("win32", "file:///", "\\")
+    uri_to_fs("win32", "file:///c:", "c:\\")
+    uri_to_fs("win32", "file:///C:/", "C:\\")
+    uri_to_fs("win32", "file:///C:/Program%20Files", "C:\\Program Files")
+    uri_to_fs("win32", "file:///C:/Program%20Files/", "C:\\Program Files\\")
+    uri_to_fs("win32", "file:///Program%20Files/", "\\Program Files\\")
+    -- http://blogs.msdn.com/ie/archive/2006/12/06/file-uris-in-windows.aspx
+    uri_to_fs("win32", "file://laptop/My%20Documents/FileSchemeURIs.doc",
+              "\\\\laptop\\My Documents\\FileSchemeURIs.doc")
+    uri_to_fs("win32",
+              "file:///C:/Documents%20and%20Settings/davris/FileSchemeURIs.doc",
+              "C:\\Documents and Settings\\davris\\FileSchemeURIs.doc")
+    -- For backwards compatibility with deprecated way of indicating drives.
+    uri_to_fs("win32", "file:///c%7C", "c:\\")
+    uri_to_fs("win32", "file:///c%7C/", "c:\\")
+    uri_to_fs("win32", "file:///C%7C/foo/", "C:\\foo\\")
+end
+
+function testcase:test_fs_to_uri_win32 ()
+    fs_to_uri("win32", "", "file:///")
+    fs_to_uri("win32", "\\", "file:///")
+    fs_to_uri("win32", "c:", "file:///c:/")
+    fs_to_uri("win32", "C:\\", "file:///C:/")
+    fs_to_uri("win32", "C:/", "file:///C:/")
+    fs_to_uri("win32", "C:\\Program Files", "file:///C:/Program%20Files")
+    fs_to_uri("win32", "C:\\Program Files\\", "file:///C:/Program%20Files/")
+    fs_to_uri("win32", "C:/Program Files/", "file:///C:/Program%20Files/")
+    fs_to_uri("win32", "\\Program Files\\", "file:///Program%20Files/")
+    fs_to_uri("win32", "\\\\laptop\\My Documents\\FileSchemeURIs.doc",
+              "file://laptop/My%20Documents/FileSchemeURIs.doc")
+    fs_to_uri("win32", "c:\\foo bar\\%2F", "file:///c:/foo%20bar/%252F")
+end
+
+function testcase:test_convert_on_unknown_os ()
+    local uri = assert(URI:new("file:///foo"))
+    assert_error("filesystem_path, unknown os",
+                 function () uri:filesystem_path("NonExistent") end)
+    assert_error("make_file_uri, unknown os",
+                 function () URIFile.make_file_uri("/foo", "NonExistent") end)
 end
 
 lunit.run()
--- uri/file.lua
+++ uri/file.lua
@@ -1,81 +1,76 @@
 local M = { _NAME = "uri.file" }
-local URI = require "uri"
-URI._subclass_of(M, "uri._generic")
-
 local Util = require "uri._util"
+local URI = require "uri"
+Util.subclass_of(M, URI)
 
-M.DEFAULT_AUTHORITY = ""
+function M.init (self)
+    if self:userinfo() or self:port() then
+        return nil, "usernames and passwords are not allowed in HTTP URIs"
+    end
 
--- Map from $^O values to implementation classes.  The Unix
--- class is the default.
-M.OS_CLASS = {
-    os2     = "OS2",
-    mac     = "Mac",
-    MacOS   = "Mac",
-    MSWin32 = "Win32",
-    win32   = "Win32",
-    msdos   = "FAT",
-    dos     = "FAT",
-    qnx     = "QNX",
-}
-
-local os_module = {}
-function M.os_class (os)
-    if not os then os = what_the_hell_operating_system_is_this() end    -- TODO
-    local class_name = "uri.file." .. (M.OS_CLASS[os] or "Unix")
-    if os_module[class_name] then return os_module[class_name] end
-    os_module[class_name] = require(class_name)
-    return os_module[class_name]
-end
-
-function M.path (self, ...) return self:path_query(...) end
-function M.host (self, ...)
-    return Util.uri_decode(self:authority(...))
-end
-
-function M.new (class, path, os) return M.os_class(os):new(path) end
-
-function M.new_abs (class, ...)
-    local file = class:new(...)
-    if file.uri:find("^file:") then
-        return file
+    local host = self:host()
+    local path = self:path()
+    if host then
+        if host:lower() == "localhost" then self:host("") end
     else
-        return file:abs(class:cwd())
-    end
-end
-
--- TODO - is there a Lua library which can give be the cwd?
---function cwd (class)
---    require Cwd;
---    my $cwd = Cwd::cwd();
---    $cwd = VMS::Filespec::unixpath($cwd) if $^O eq 'VMS';
---    $cwd = $class->new($cwd);
---    $cwd .= "/" unless substr($cwd, -1, 1) eq "/";
---    return $cwd;
---end
-
-function M.canonical (self)
-    local other = M._SUPER.canonical(self)
-
-    local scheme = other:scheme()
-    local auth = other:authority()
-    if not scheme and not auth then return other end    -- relative
-
-    if not auth or auth == "" or auth:lower() == "localhost" or
-       (DEFAULT_AUTHORITY and auth:lower() == DEFAULT_AUTHORITY:lower()) then
-        -- avoid cloning if $auth already match
-        if (auth or DEFAULT_AUTHORITY) and
-           (not auth or not DEFAULT_AUTHORITY or auth ~= DEFAULT_AUTHORITY) then
-            if self == other then other = URI:new(other) end
-            other:authority(DEFAULT_AUTHORITY)
+        if not path:find("^/") then
+            return nil, "file URIs must contain a host, even if it's empty"
         end
+        self:host("")
     end
 
-    return other
+    if path == "" then self:path("/") end
+
+    return self
 end
 
-function M.file (self, os) return M.os_class(os):file(self) end
-function M.dir (self, os)  return M.os_class(os):dir(self)  end
+function M.host (self, ...)
+    local old = M._SUPER.host(self)
+
+    if select('#', ...) > 0 then
+        local new = ...
+        if not new then error("file URIs must have an authority part") end
+        if new:lower() == "localhost" then new = "" end
+        M._SUPER.host(self, new)
+    end
+
+    return old
+end
+
+function M.path (self, ...)
+    local old = M._SUPER.path(self)
+
+    if select('#', ...) > 0 then
+        local new = ...
+        if not new or new == "" then new = "/" end
+        M._SUPER.path(self, new)
+    end
+
+    return old
+end
+
+local function _os_implementation (os)
+    local FileImpl = Util.attempt_require("uri.file." .. os:lower())
+    if not FileImpl then
+        error("no file URI implementation for operating system " .. os)
+    end
+    return FileImpl
+end
+
+function M.filesystem_path (self, os)
+    return _os_implementation(os).filesystem_path(self)
+end
+
+function M.make_file_uri (path, os)
+    return _os_implementation(os).make_file_uri(path)
+end
+
+for _, method in ipairs({ "userinfo", "port" }) do
+    M[method] = function (self, new)
+        if new then error("file URIs may not have " .. method) end
+        return nil
+    end
+end
 
 return M
 -- vi:ts=4 sw=4 expandtab
