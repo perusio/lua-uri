ca4a02e450d93052d00a12cb3941612d3e5214da
Geoff Richards <geoff@geoffrichards.co.uk>
2007-11-29 18:15:31 +0000

    Move test data into test program file for simplicity
    I thought I was going to use this data in more than one test program, and
    it was originally a text file, but now that it's Lua code only used in one
    place I might as well fold it back into the test code.  This leaves me with
    no extra test data files, which is nice.

--- test/_resolve.lua
+++ test/_resolve.lua
@@ -2,6 +2,60 @@ require "uri-test"
 local URI = require "uri"
 local testcase = TestCase("Test 'resolve' and 'relativize' methods")
 
+-- Test data from RFC 3986.  The 'http' prefix has been changed throughout
+-- to 'x-foo' so as not to trigger any scheme-specific normalization.
+local resolve_tests = {
+    -- 5.4.1.  Normal Examples
+    ["g:h"]             = "g:h",
+    ["g"]               = "x-foo://a/b/c/g",
+    ["./g"]             = "x-foo://a/b/c/g",
+    ["g/"]              = "x-foo://a/b/c/g/",
+    ["/g"]              = "x-foo://a/g",
+    ["//g"]             = "x-foo://g",
+    ["?y"]              = "x-foo://a/b/c/d;p?y",
+    ["g?y"]             = "x-foo://a/b/c/g?y",
+    ["#s"]              = "x-foo://a/b/c/d;p?q#s",
+    ["g#s"]             = "x-foo://a/b/c/g#s",
+    ["g?y#s"]           = "x-foo://a/b/c/g?y#s",
+    [";x"]              = "x-foo://a/b/c/;x",
+    ["g;x"]             = "x-foo://a/b/c/g;x",
+    ["g;x?y#s"]         = "x-foo://a/b/c/g;x?y#s",
+    [""]                = "x-foo://a/b/c/d;p?q",
+    ["."]               = "x-foo://a/b/c/",
+    ["./"]              = "x-foo://a/b/c/",
+    [".."]              = "x-foo://a/b/",
+    ["../"]             = "x-foo://a/b/",
+    ["../g"]            = "x-foo://a/b/g",
+    ["../.."]           = "x-foo://a/",
+    ["../../"]          = "x-foo://a/",
+    ["../../g"]         = "x-foo://a/g",
+
+    -- 5.4.2.  Abnormal Examples
+    ["../../../g"]      = "x-foo://a/g",
+    ["../../../../g"]   = "x-foo://a/g",
+    ["/./g"]            = "x-foo://a/g",
+    ["/../g"]           = "x-foo://a/g",
+    ["g."]              = "x-foo://a/b/c/g.",
+    [".g"]              = "x-foo://a/b/c/.g",
+    ["g.."]             = "x-foo://a/b/c/g..",
+    ["..g"]             = "x-foo://a/b/c/..g",
+    ["./../g"]          = "x-foo://a/b/g",
+    ["./g/."]           = "x-foo://a/b/c/g/",
+    ["g/./h"]           = "x-foo://a/b/c/g/h",
+    ["g/../h"]          = "x-foo://a/b/c/h",
+    ["g;x=1/./y"]       = "x-foo://a/b/c/g;x=1/y",
+    ["g;x=1/../y"]      = "x-foo://a/b/c/y",
+    ["g?y/./x"]         = "x-foo://a/b/c/g?y/./x",
+    ["g?y/../x"]        = "x-foo://a/b/c/g?y/../x",
+    ["g#s/./x"]         = "x-foo://a/b/c/g#s/./x",
+    ["g#s/../x"]        = "x-foo://a/b/c/g#s/../x",
+    ["x-foo:g"]         = "x-foo:g",
+
+    -- Some extra tests for good measure
+    ["#foo?"]           = "x-foo://a/b/c/d;p?q#foo?",
+    ["?#foo"]           = "x-foo://a/b/c/d;p?#foo",
+}
+
 local function test_abs_rel (base, uref, expect)
     local bad = false
 
@@ -43,8 +97,7 @@ function testcase:test_resolve ()
     local testno = 1
     local bad = false
 
-    local tests = require("test.data.abs-data")
-    for rel, abs in pairs(tests) do
+    for rel, abs in pairs(resolve_tests) do
         if test_abs_rel(base, rel, abs) then bad = true end
     end
 
